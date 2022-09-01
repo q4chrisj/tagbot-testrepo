31cfc74f7deb243ddcc7e76766ca2cc15d133e7a
Chris Jones <chrisj@q4inc.com>
2022-09-01 15:14:55 -0400

    testing no tags, create major
    Signed-off-by: Chris Jones <chrisj@q4inc.com>

--- src/index.js
+++ src/index.js
@@ -1,7 +1,2 @@
 console.log("hi");
 console.log("demo");
-console.log("another demo line");
-console.log("add");
-console.log("testing 123");
-console.log("one more line");
-console.log("another line");
